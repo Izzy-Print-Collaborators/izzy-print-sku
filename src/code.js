b3a8e93e59511db40e58f4948d84b1ec37b81064
Mateus <mateusc.ramos@outlook.com>
2023-08-12 02:31:44 -0300

    javascript em arquivo separado

--- src/code.js
+++ src/code.js
@@ -1,68 +1,98 @@
 
-//nav -> node -> express -> middleware -> function
+function gerarSKU() {
+    let nomeEstampa = document.getElementById("estampa").value;
+    nomeEstampa = nomeEstampa.replace(/\s/g, '');
 
-//middleware
+    let char = nomeEstampa.charAt(nomeEstampa.length - 1);
 
-
-// caminho = __dirname(pega o diretorio local)/pages/registro.html
-
-
-//---- codigo noss abaixo
-    const express = require("express");
-    const path = require("path");
-    const app = express ();
-    app.use(express.static('src'));
-
-    app.use(express.urlencoded());
-    app.get("/formulario", formulario); //não pode ser o mesmo nome do que o post
-    app.post("/formulario", processarFormulario);
-    app.get("/login", login);
-    app.post("/login", processarLogin);
-
-    function formulario(req, res){
-        const caminho = path.join(__dirname,"src", "pages","formulario.html");
-        res.sendFile(caminho);
+    if (nomeEstampa.length > 8) {
+        nomeEstampa = nomeEstampa.substring(0, 8).toUpperCase();
+    } else {
+        nomeEstampa = nomeEstampa.toUpperCase();
+        for (let i = nomeEstampa.length; i < 8; i++) {
+            nomeEstampa += char.toUpperCase();
+        }
     }
 
-    function gerarSKU(req, skuCor) {
-        if (skuCor != null){
-            let cor = skuCor;
-            tamanhos.forEach(function(tamanho){
-                let i = 0;
-                if (tamanho =! "EG" ){ //arrumar!!!!!!!!!
-                    let skuTemp = [];
-                    skuTemp[i]  = nomeEstampa + "-A2-" + tipoImpressao + local + modelagem + cor + tamanho;
-                    console.log(skuBranco[i]);
+    let local = document.querySelector('input[name=local]:checked').value;
+    let modelagem = document.querySelector('input[name=modelagem]:checked').value;
+    let tipoImpressao = '';
+
+    let tamanhos = [];
+
+    if (modelagem == 'ST') {
+        tamanhos = ['P', 'PP', 'M', 'G', 'GG', 'EG', 'G1', 'G2'];
+
+    } else if (modelagem == 'CL') {
+        tamanhos = ['P', 'PP', 'M', 'G', 'GG', 'EG'];
+
+    } else if (modelagem == 'BB') {
+        tamanhos = ['P', 'PP', 'M', 'G', 'GG'];
+
+    } else if (modelagem == 'IN') {
+        tamanhos = ['02', '04', '06', '08', '10'];
+
+    } else {
+        tamanhos = ['P', 'PP', 'M', 'G', 'GG', 'EG'];
+    }
+
+    let sku = [];
+
+    const cores = [
+        { key: 'branca', allowClara: true, allowEG: true },
+        { key: 'preta', allowClara: false, allowEG: true },
+        { key: 'cinzaMescla', allowClara: false, allowEG: false },
+        { key: 'estonadaChumbo', allowClara: false, allowEG: false },
+        { key: 'estonadaMarrom', allowClara: false, allowEG: false },
+        { key: 'lilas', allowClara: false, allowEG: false },
+        { key: 'marrom', allowClara: false, allowEG: false },
+        { key: 'offWhite', allowClara: true, allowEG: true },
+        { key: 'verdeMusgo', allowClara: false, allowEG: false },
+        { key: 'verde', allowClara: false, allowEG: false },
+        { key: 'vermelha', allowClara: false, allowEG: false },
+        { key: 'azulMarinho', allowClara: false, allowEG: false },
+        { key: 'azulClaro', allowClara: false, allowEG: false }
+    ];
+
+    cores.forEach(corConfig => {
+        const corMarcada = document.getElementById(corConfig.key).checked;
+        const cor = document.getElementById(corConfig.key).value;
+        if (corMarcada) {
+            let skuCor = [];
+
+            if (corConfig.allowClara) {
+                tipoImpressao = 'CL';
+            } else {
+                tipoImpressao = 'ES';
+            }
+
+            tamanhos.forEach(tamanho => {
+                if (corConfig.allowEG || tamanho != 'EG') {
+                    const skuItem = nomeEstampa + "-A2-" + tipoImpressao + local + modelagem + cor + tamanho;
+                    skuCor.push(skuItem);
                 }
-                i++
-            })
-            sku = sku.concat(skuTemp)
+            });
+
+            sku = sku.concat(skuCor);
         }
-    }
+    });
 
+    sku.forEach(codigo => {
+        const skuContainer = document.getElementById("skuContainer");
+        skuContainer.innerHTML += `<p>${codigo} <button onclick="copiarSKU('${codigo}')" class="botao-copiar">Copiar</button></p>`;
+    });        
+}
 
+function limparSKUs() {
+    const skuContainer = document.getElementById("skuContainer");
+    skuContainer.innerHTML = ''; 
+}
 
-    function login(req, res) {
-        res.status(303)
-        const caminho = path.join(__dirname, "src", "pages", "login.html");
-        res.sendFile(caminho);
-    }
-    
-    function processarLogin(req, res) {
-        const codigo = req.body.codigo;
-        
-    
-        if(codigo == 'SP3065791' || codigo == 'mateusmeuamor'){
-            res.status(301);
-            res.header("Location", "/formulario");
-            res.send();
-        }
-        else{
-            res.status(302);
-            res.header("Location", "/login")
-            res.send("Digite um código valido")
-        }
-        
-    }
-
-    app.listen(3001, () => console.log("Rodando na porta 3001"));
+function copiarSKU(sku) {
+    const textarea = document.createElement('textarea');
+    textarea.value = sku;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+}
