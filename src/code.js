b75ed6b926a79070042abc478441369271b8e009
João A . Haupt <127232326+JoaoAHaupt@users.noreply.github.com>
2026-08-18 13:56:21 -0300

    Refactor nomeEstampa normalization and sanitization
    Refactor normalization and sanitization of 'nomeEstampa' to improve readability.

--- src/code.js
+++ src/code.js
@@ -87,7 +87,11 @@ function gerarSKU() {
     let nomeEstampa = document.getElementById("estampa").value;
     document.getElementById("nome_camiseta").innerText = nomeEstampa;
     
-    nomeEstampa = nomeEstampa.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/ç/g, "c").replace(/[^a-zA-Z0-9 ]/g, "");   // remove tudo que não seja letra, número ou espaço
+    nomeEstampa = nomeEstampa
+     .normalize("NFD")
+     .replace(/[\u0300-\u036f]/g, "")
+     .replace(/ç/g, "c")
+     .replace(/[^a-zA-Z0-9]/g, "");   // remove tudo que não seja letra, número ou espaço
     
    
     let char = nomeEstampa.charAt(nomeEstampa.length - 1);
